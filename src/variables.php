de239257f96efa898f55b9a75018d4f73d6a1b23
getsadzeg <getsadzeguram@gmail.com>
2016-08-17 00:09:16 +0400

    add string & converting types from one to another sample

--- src/variables.php
+++ src/variables.php
@@ -3,4 +3,10 @@ $int_var = 122;
 $second_int = 135;
 echo $int_var + $second_int;
 echo "\n";
+$description = "about variables";
+echo "this file is $description" ;
+echo "\n";
+$test_var = 5;
+$test_var = true; //$test_var is now boolean
+if($test_var) echo "test variable is now boolean \n";
 ?>
